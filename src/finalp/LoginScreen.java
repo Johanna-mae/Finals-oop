72bc14ae7c4e67992252ea657910e1f68fb0587a
Valiant49 <vichandreicortez49@gmail.com>
2025-11-30 21:40:03 +0800

    improved UI; removed old commented out code

--- src/finalp/LoginScreen.java
+++ src/finalp/LoginScreen.java
@@ -4,46 +4,64 @@ import java.awt.*;
 import javax.swing.*;
 
 
+
 class LoginScreen extends JFrame{
     public LoginScreen(){
         add(createLoginPanel());
-        //add(backgroundImage());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-
-    public JLabel backgroundImage() {
-        JLabel bgImg = new JLabel();
-        ImageIcon bgImageIcon = new ImageIcon("scott-graham-OQMZwNd3ThU-unsplash.jpg");
-        bgImg.setIcon(bgImageIcon);
-        bgImg.setLayout(new BorderLayout());
-
-        return bgImg;
-    }
-    
 
     private JPanel createLoginPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(null);
-    
+         
+        ImageIcon originalImage = new ImageIcon(getClass().getResource("images/bg-loan-image.png"));
+        Image scaledImage = originalImage.getImage().getScaledInstance(960, 720, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        JLabel backgroundImageJLabel = new JLabel(scaledIcon);
+        backgroundImageJLabel.setBounds(0, 0, 960, 720);
+        backgroundImageJLabel.setLayout(null);
+
+        JPanel boundingBox = new JPanel();
+        boundingBox.setBounds(130, 110, 700, 500);
+        boundingBox.setBackground(new Color(255, 255, 255, 215));
+        boundingBox.setLayout(null);
+
+        JLabel title = new JLabel("PaLoan System");
+        title.setBounds(170, 65, 500, 56);
+        title.setFont(new Font("Sans Serif", Font.BOLD, 50));
+
         JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(10, 10, 150, 30);
+        usernameLabel.setBounds(190, 200, 165, 40);
+        usernameLabel.setFont(new Font("Sans Serif", Font.PLAIN, 24));       
+        
         JTextField usernameField = new JTextField();
-        usernameField.setBounds(10, 45, 150, 30);
+        usernameField.setBounds(360, 205, 150, 30);
+        
         JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10, 75, 150, 30);
-        JTextField passwordField = new JTextField();
-        passwordField.setBounds(10, 105, 150, 30);
-        JButton loginButton = new JButton("Login");
-        loginButton.setBounds(10, 140, 75, 30);
+        passwordLabel.setBounds(190, 250, 165, 40);
+        passwordLabel.setFont(new Font("Sans Serif", Font.PLAIN, 24));
+        
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(360, 255, 150, 30);
+        
+        JButton loginButton = new JButton("LOGIN");
+        loginButton.setBounds(220, 380, 275, 64);
+        loginButton.setFont(new Font("Sans Serif", Font.BOLD, 24));
+        
+        //panel.add(backgroundImageJLabel);
+        boundingBox.add(title);
+        boundingBox.add(usernameLabel);
+        boundingBox.add(usernameField);
+        boundingBox.add(passwordLabel);
+        boundingBox.add(passwordField);
+        boundingBox.add(new JLabel());
+        boundingBox.add(loginButton);
 
-        panel.add(usernameLabel);
-        panel.add(usernameField);
-        panel.add(passwordLabel);
-        panel.add(passwordField);
-        panel.add(new JLabel());
-        panel.add(loginButton);
+        backgroundImageJLabel.add(boundingBox);
 
-        panel.setBackground(Color.red);
+        panel.add(backgroundImageJLabel);
 
         return panel;
     }
